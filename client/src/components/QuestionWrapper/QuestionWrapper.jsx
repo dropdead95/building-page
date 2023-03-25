0e4737ac96d72cbc81a73f3809cebd42f2638e5b
Kravchuk Anton <Kravch-anton@mail.ru>
2023-03-25 21:23:55 +0300

    init

--- client/src/components/QuestionWrapper/QuestionWrapper.jsx
+++ client/src/components/QuestionWrapper/QuestionWrapper.jsx
@@ -25,12 +25,13 @@ export const QuestionWrapper = () => {
       try {
         const res = await axios.get(
           process.env.REACT_APP_API_URL + "/questions?populate=*",
-          {
-            headers: {
-              Authorization: `bearer ${process.env.REACT_APP_API_TOKEN}`,
-            },
-          }
+            {
+              headers: {
+                Authorization: `bearer 1726eddde48a0f8ee1a2b5ad4d00d8a7dae81294fdfda779339a8f7488073d899e4f50fc148a42e42ca7450f7b9e174e239abcb92862e92eb8feea931ec666b3a7a112272eaf5ce53c537f8fbb6126f9c502df4cc2083a9406dd432abf62bcf0f0b60a4ab5f143ea6199b9ac3cd2448ae76c084216543550f9f03b66ac221cc2`,
+              },
+            }
         );
+
         setQuestions(res.data.data);
         console.log(questions);
       } catch (err) {
