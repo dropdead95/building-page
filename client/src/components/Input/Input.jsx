554b95d51d8921088eb8df36ec124ea600616306
Kravchuk Anton <Kravch-anton@mail.ru>
2023-03-27 22:23:51 +0300

    input

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './Input.css'
 import {useDispatch} from "react-redux";
 import {setAnswer} from "../../bll/AnswerReducer";
 
 export const Input = ({data}) => {
+    const [value, setValue] = useState('')
+    const setInputValue = (e) => {
+         dispatch(setAnswer({title:data.answer, answer :e.currentTarget.value}))
+        setValue('')
+    }
     const dispatch = useDispatch()
-    console.log(data)
+
     return <div className="text-field">
         <label className="text-field__label" htmlFor="username">{data.answer}</label>
-        <input onBlur={(e) => dispatch(setAnswer({title:data.answer, answer :e.currentTarget.value}))} className="text-field__input" type="text" name="username" id="username"
-               placeholder={data.answer}/>
+        <input onBlur={setInputValue} onChange={e => setValue(e.currentTarget.value)} className="text-field__input" type="text" name="username" id="username"
+               placeholder={data.answer} value={value}/>
     </div>
 }
