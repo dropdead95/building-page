65e763efaf8a65e2e03c430eec0533834e693df3
Kravchuk Anton <Kravch-anton@mail.ru>
2023-03-30 17:01:20 +0300

    project merging

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 
-import {Layout, Main} from "../../pages";
+import {Layout, Main, Meeting} from "../../pages";
 import {Gratitude, Quiz, QuizBuilding} from "../../components";
 import {PhotoContainer} from "../../components/PhotoGallery/PhotoContainer";
-import { Layout, Main, Meeting } from "../../pages";
-import { Gratitude, Quiz, QuizBuilding } from "../../components";
+
 
 export const App = () => {
 
@@ -14,28 +13,16 @@ export const App = () => {
             <Route path="/" element={<Layout/>}>
                 <Route index element={<Main/>}/>
                 <Route path={"quiz"} element={<QuizBuilding/>}>
-                    <Route path=":id" element={<Quiz />}/>
+                    <Route path=":id" element={<Quiz/>}/>
                     <Route path=":id/gratitude" element={<Gratitude/>}/>
 
                 </Route>
-                <Route path="gallery" element={<PhotoContainer />}/>
-                <Route path="meeting" element={<Meeting />} />
-
+                <Route path="gallery" element={<PhotoContainer/>}/>
+                <Route path="meeting" element={<Meeting/>}/>
 
 
             </Route>
         </Routes>
     );
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Main />} />
-        <Route path={"quiz"} element={<QuizBuilding />}>
-          <Route path=":id" element={<Quiz />} />
-        </Route>
-        <Route path="gratitude" element={<Gratitude />} />
-        <Route path="meeting" element={<Meeting />} />
-      </Route>
-    </Routes>
-  );
+
 };
