e967c59fdbbf49943ed842909fc6eb1df9a113c8
Kravchuk Anton <Kravch-anton@mail.ru>
2023-04-02 12:53:21 +0300

    feature: photo gallery

--- client/src/components/PhotoGallery/PhotoList.jsx
+++ client/src/components/PhotoGallery/PhotoList.jsx
@@ -1,22 +1,47 @@
 import React, {useState} from "react";
 import styles from "./photo.module.scss";
 import {PhotoItem} from "./PhotoItem";
+import "yet-another-react-lightbox/styles.css";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+
 
 export const PhotoList = ({folder}) => {
-    const [toggler, setToggler] = useState(false);
-    console.log(folder)
-    return <div className={styles.photoListsWrapper}> {folder.map((t, index) => <div key={index}><PhotoItem
-        dataItem={t}/></div>)}
-        <button onClick={() => setToggler(!toggler)}>
-            Toggle Lightbox
-        </button>
-        {/*<FsLightbox
-            toggler={toggler}
-            sources={[
-                'https://i.imgur.com/fsyrScY.jpg',
-                'https://www.youtube.com/watch?v=3nQNiWdeH2Q',
-                'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-            ]}
-        />*/}</div>
+    const [open, setOpen] = useState(false)
+    const [indexSlides, setIndexSlides] = useState(0)
+    const openLightBox = (index) => {
+        setOpen(true)
+        setIndexSlides(index)
+
+    }
+    const thumbnailsRef = React.useRef(null);
+    console.log(indexSlides)
+    let src = 'src'
+    let slides = []
+    folder[indexSlides].images.data.map(t => slides.push({[src]: `${process.env.REACT_APP_UPLOAD_URL}${t.attributes.url}`}))
+
+
+    return <div className={styles.photoListsWrapper}> {folder.map((t, index) => <div key={index}
+                                                                                     onClick={() => openLightBox(index)}>
+        <PhotoItem
+            dataItem={t}/></div>)}
+
+
+        <Lightbox
+            open={open}
+            close={() => setOpen(false)}
+            slides={slides}
+            plugins={[Thumbnails]}
+            thumbnails={{ref: thumbnailsRef}}
+            on={{
+                click: () => {
+                    (thumbnailsRef.current?.visible
+                        ? thumbnailsRef.current?.hide
+                        : thumbnailsRef.current?.show)?.();
+                },
+            }}
+        />
+    </div>
 }
 
