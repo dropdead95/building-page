35b5d0b87b9763391ffef8f133557a3c721a6e64
Kravchuk Anton <Kravch-anton@mail.ru>
2023-04-02 13:12:35 +0300

    changes styles

--- client/src/components/PhotoGallery/PhotoItem.jsx
+++ client/src/components/PhotoGallery/PhotoItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./photo.module.scss";
 
 export const PhotoItem = ({dataItem}) => {
-    return <div className={styles.photoItem}><img className={styles.mainPhoto}
+    return <div className={styles.photoItem}><img
         src={`${process.env.REACT_APP_UPLOAD_URL}${dataItem.mainPhoto.data?.attributes?.url}`} alt="photo"/>
         <span>{dataItem.title}</span>
     </div>
