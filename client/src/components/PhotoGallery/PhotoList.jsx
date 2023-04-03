05cd7e75bc8765184ce23ce7d16a62a03ae3bfce
Kravchuk Anton <Kravch-anton@mail.ru>
2023-04-03 07:17:41 +0300

    feature: pagination in photo gallery

--- client/src/components/PhotoGallery/PhotoList.jsx
+++ client/src/components/PhotoGallery/PhotoList.jsx
@@ -5,7 +5,8 @@ import "yet-another-react-lightbox/styles.css";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-
+import {usePagination, Pagination} from "pagination-react-js"
+import './pagination.css'
 
 export const PhotoList = ({folder}) => {
     const [open, setOpen] = useState(false)
@@ -16,17 +17,53 @@ export const PhotoList = ({folder}) => {
 
     }
     const thumbnailsRef = React.useRef(null);
-    console.log(indexSlides)
+    const {currentPage, entriesPerPage, entries} = usePagination(1, 6);
     let src = 'src'
     let slides = []
     folder[indexSlides].images.data.map(t => slides.push({[src]: `${process.env.REACT_APP_UPLOAD_URL}${t.attributes.url}`}))
 
 
-    return <div className={styles.photoListsWrapper}> {folder.map((t, index) => <div key={index}
-                                                                                     onClick={() => openLightBox(index)}>
-        <PhotoItem
-            dataItem={t}/></div>)}
+    return <div className={styles.photoListsWrapper}>
+        <div className={styles.photoList}>
+            {folder.slice(entries.indexOfFirst, entries.indexOfLast).map((t, index) => <div key={index}
+                                                                                            onClick={() => openLightBox(index)}>
+                <PhotoItem
+                    dataItem={t}/>
 
+            </div>)}
+        </div>
+
+        <Pagination
+            entriesPerPage={entriesPerPage.get}
+            totalEntries={folder.length}
+            currentPage={{get: currentPage.get, set: currentPage.set}}
+            offset={3}
+            classNames={{
+                wrapper: "pagination m-auto",
+                item: "pagination-item",
+                itemActive: "pagination-item-active",
+                navPrev: "pagination-item nav-item",
+                navNext: "pagination-item nav-item",
+                navStart: "pagination-item nav-item",
+                navEnd: "pagination-item nav-item",
+                navPrevCustom: "pagination-item",
+                navNextCustom: "pagination-item"
+            }}
+            showFirstNumberAlways={true}
+            showLastNumberAlways={true}
+            navStart="&#171;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+            navEnd="&#187;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+            navPrev="&#x2039;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+            navNext="&#x203a;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+            navPrevCustom={{
+                steps: 5,
+                content: "\u00B7\u00B7\u00B7" /* Here you can pass anything (Text, HTML Tag, React Component, ...) */
+            }}
+            navNextCustom={{
+                steps: 5,
+                content: "\u00B7\u00B7\u00B7" /* Here you can pass anything (Text, HTML Tag, React Component, ...) */
+            }}
+        />
 
         <Lightbox
             open={open}
