abfe8a1533c3ef76c1bc7831ac10c4ac68cd3681
Andrey Lisovsky <mitolcore1234@gmail.com>
2023-04-04 13:24:24 +0300

    still creating meeting page

--- client/src/components/MenuButton/MenuButton.jsx
+++ client/src/components/MenuButton/MenuButton.jsx
@@ -2,11 +2,32 @@ import React from "react";
 import classNames from "classnames";
 
 import styles from "./MenuButton.module.scss";
+import { Lock } from "../../icons";
 
-export const MenuButton = ({ title, className, children, disabled }) => {
+export const MenuButton = ({
+  title,
+  className,
+  children,
+  disabled,
+  lockText,
+  classNameForBtn,
+}) => {
   return (
-    <button disabled={disabled} className={classNames(styles.button, className)}>
-      {children} {title}
-    </button>
+    <div className={classNames(styles.wrapper, classNameForBtn)}>
+      <button
+        disabled={disabled}
+        className={classNames(styles.button, className)}
+      >
+        {children} {title}
+      </button>
+      {lockText && (
+        <div className={styles.lockWrapper}>
+          <Lock />
+          <p className={styles.text}>
+            Ваши данные не будут переданы третьим лицам
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
