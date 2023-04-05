5ee1dbc5d05bb805a6b5a9a4ace8f3e3b55e7612
Kravchuk Anton <Kravch-anton@mail.ru>
2023-04-06 00:12:23 +0300

    fix search

--- client/src/components/PhotoGallery/PhotoContainer.jsx
+++ client/src/components/PhotoGallery/PhotoContainer.jsx
@@ -9,7 +9,7 @@ import {useDispatch, useSelector} from "react-redux";
 export const PhotoContainer = () => {
     const [folder, setFolder] = useState(0);
     const [search, setSearch] = useState('')
-        const dispatch = useDispatch();
+    const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchPhotoGallery());
     }, []);
@@ -18,31 +18,30 @@ export const PhotoContainer = () => {
     data?.map(t => (t.attributes.photo.filter(item => item.title?.toLowerCase().includes(search) ? Arr.push(item) : '')))
     console.log(search);
     let dataTransfer
-    if(search.length > 1) {
+    if (search.length > 1) {
         dataTransfer = Arr
-    }
-    else if(data)  {
+    } else if (data) {
         dataTransfer = data[folder]?.attributes?.photo
     }
 
-    console.log(dataTransfer)
+    console.log(Arr.length < 1)
     return (
         <section className={styles.photogallery}>
-            {dataTransfer && (
-                <Container>
-                    <h2 className={styles.title}>Фотогалерея</h2>
-                    <div className={styles.wrapper}>
-                        <NavigationContainer
-                            setSearch={setSearch}
-                            search={search}
-                            data={data}
-                            folder={folder}
-                            setFolder={setFolder}
-                        />
-                        <PhotoList key={dataTransfer[0]?.id} folder={dataTransfer}/>
-                    </div>
-                </Container>
-            )}
+
+            <Container>
+                <h2 className={styles.title}>Фотогалерея</h2>
+                <div className={styles.wrapper}>
+                    <NavigationContainer
+                        setSearch={setSearch}
+                        search={search}
+                        data={data}
+                        folder={folder}
+                        setFolder={setFolder}
+                    />
+                    {Arr.length > 0 && (<PhotoList key={dataTransfer[0]?.id} folder={dataTransfer}/>)}
+                </div>
+            </Container>
+
         </section>
     );
 };
